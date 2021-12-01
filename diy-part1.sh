92ec56ff1859ca245f066211c2a93f8ad47a3161
lisakhan <92414116+lisakhan@users.noreply.github.com>
2021-12-01 10:53:27 +0800

    Update diy-part1.sh
    add lisa version of smartdns

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,7 +16,7 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # Add a feed source
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-echo git clone --depth 1 https://github.com/lisakhan/smartdns.git feeds/small/smartdns
-#echo 'src-git smartdns https://github.com/lisakhan/smartdns' >>feeds.conf.default
+echo 'src-git lisasmart https://github.com/lisakhan/smartdns' >>feeds.conf.default
+#echo git clone --depth 1 https://github.com/lisakhan/smartdns.git feeds/package/net/smartdns
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
